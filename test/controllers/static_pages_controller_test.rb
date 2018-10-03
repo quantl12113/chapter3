2c935b0f598d79c140299e224a073ca0e60df7c5
Quan Le <quantl12113@gmail.com>
2018-10-03 16:12:28 +0700

    Finish chapter 3,4,5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,12 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | Ruby on rails"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Ruby on rails"
   end
 end
